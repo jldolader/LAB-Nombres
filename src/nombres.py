80274b94f7bab660ff7a4509bb42915630148ff3
jldolader <juanlumsevilla@gmail.com>
2025-01-12 21:13:18 +0100

    ✅ Métodos y tests creados/probados para calcular_nombres, calcular_top_nombres_de_año, calcular_nombres_ambos_generos, calcular_nombres_compuestos, calcular_frecuencia_media_nombre_años, calcular_nombre_mas_frecuente_año_genero, calcular_año_mas_frecuencia_nombre

--- src/nombres.py
+++ src/nombres.py
@@ -1,7 +1,6 @@
 from collections import namedtuple
 import csv
 
-
 FrecuenciaNombre = namedtuple('FrecuenciaNombre', 'año,nombre,frecuencia,genero')
 
 def leer_frecuencias_nombres(ruta_fichero: str) -> list[FrecuenciaNombre]:
@@ -13,47 +12,31 @@ def leer_frecuencias_nombres(ruta_fichero: str) -> list[FrecuenciaNombre]:
                 nombre = str(d[1]),
                 frecuencia = int(d[2]),
                 genero = str(d[3])
-            ) 
-            for d in reader
-        ]
+            ) for d in reader]
         
 def filtrar_por_genero(lfn: list[FrecuenciaNombre], genero: str) -> list[FrecuenciaNombre]:
     return [f for f in lfn if f.genero == genero]
 
 def calcular_nombres(lfn: list[FrecuenciaNombre], genero: str|None = None) -> set[str]:
-    pass
+    return set(f.nombre for f in lfn if genero is None or f.genero == genero)
 
 def calcular_top_nombres_de_año(lfn: list[FrecuenciaNombre], anyo: int, lim: int = 10, genero: str|None = None) -> list[tuple[str, int]]:
-    pass
+    return sorted((f.nombre, f.frecuencia) for f in lfn if f.año == anyo and (genero is None or f.genero == genero))[:lim]
 
-'''
-def calcular_nombres_ambos_generos() -> :
-    pass
+def calcular_nombres_ambos_generos(lfn: list[FrecuenciaNombre]) -> set[str]:
+    return {f.nombre for f in lfn if f.genero == 'Hombre'} & {f.nombre for f in lfn if f.genero == 'Mujer'}
 
-def calcular_nombres_compuestos() -> :
-    pass
+def calcular_nombres_compuestos(lfn: list[FrecuenciaNombre], genero: str|None = None) -> set[str]:
+    return set(f.nombre for f in lfn if ' ' in f.nombre and (genero is None or f.genero == genero))
 
-def calcular_frecuencia_media_nombre_años() -> :
-    pass
+def calcular_frecuencia_media_nombre_años(lfn: list[FrecuenciaNombre], nombre: str, anyo_ini: int, anyo_fin: int) -> float:
+    frecuencia = [f.frecuencia for f in lfn if f.nombre.lower() == nombre.lower() and anyo_ini <= f.año <= anyo_fin]
+    return sum(frecuencia) / len(frecuencia) if frecuencia else 0.0
 
-def calcular_nombre_mas_frecuente_año_genero() -> :
-    pass
+def calcular_nombre_mas_frecuente_año_genero(lfn: list[FrecuenciaNombre], anyo: int, genero: str) -> str:
+    lista = [f for f in lfn if f.año == anyo and f.genero.lower() == genero.lower()]
+    return max(lista, key = lambda f: f.frecuencia).nombre if lista else ''
 
-def calcular_año_mas_frecuencia_nombre() -> :
-    pass
-
-def calcular_nombres_mas_frecuentes() -> :
-    pass
-
-def calcular_año_frecuencia_por_nombre() -> :
-    pass
-
-def calcular_nombre_mas_frecuente_por_año() -> :
-    pass
-
-def calcular_frecuencia_por_año() -> :
-    pass
-
-def mostrar_evolucion_por_año() -> :
-    pass
-'''
+def calcular_año_mas_frecuencia_nombre(lfn: list[FrecuenciaNombre], nombre: str) -> int:
+    lista = [f for f in lfn if f.nombre.lower() == nombre.lower()]
+    return max(lista, key = lambda f: f.frecuencia).año if lista else 0
--- src/nombres_test.py
+++ src/nombres_test.py
@@ -3,17 +3,37 @@ from nombres import *
 def test_leer_frecuencias_nombres(ruta_fichero):
     return leer_frecuencias_nombres(ruta_fichero)
 
-def test_filtrar_por_genero(lfn: list[FrecuenciaNombre], genero: str) -> list[FrecuenciaNombre]:
+def test_filtrar_por_genero(lfn, genero):
     print(filtrar_por_genero(lfn, genero))
 
-def test_calcular_nombres(lfn: list[FrecuenciaNombre], genero: str|None = None) -> set[str]:
+def test_calcular_nombres(lfn, genero = None):
     print(calcular_nombres(lfn, genero))
 
-def test_calcular_top_nombres_de_año(lfn: list[FrecuenciaNombre], anyo: int, lim: int = 10, genero: str|None = None) -> list[tuple[str, int]]:
+def test_calcular_top_nombres_de_año(lfn, anyo, lim = 10, genero = None):
     print(calcular_top_nombres_de_año(lfn, anyo, lim, genero))
 
+def test_calcular_nombres_ambos_generos(lfn):
+    print(calcular_nombres_ambos_generos(lfn))
+
+def test_calcular_nombres_compuestos(lfn, genero = None):
+    print(calcular_nombres_compuestos(lfn, genero))
+    
+def test_calcular_frecuencia_media_nombre_años(lfn, nombre, anyo_ini, anyo_fin) -> float:
+    print(calcular_frecuencia_media_nombre_años(lfn, nombre, anyo_ini, anyo_fin))
+    
+def test_calcular_nombre_mas_frecuente_año_genero(lfn, anyo, genero):
+    print(calcular_nombre_mas_frecuente_año_genero(lfn, anyo, genero))
+    
+def test_calcular_año_mas_frecuencia_nombre(lfn, nombre):
+    print(calcular_año_mas_frecuencia_nombre(lfn, nombre))
+    
 if __name__ == '__main__':
     lfn = test_leer_frecuencias_nombres('data/frecuencias_nombres.csv')
-    test_filtrar_por_genero(lfn, 'Hombre')
-    #test_calcular_nombres(frn, 'Mujer')
-    #test_calcular_top_nombres_de_año(frn, 2005, 4)
+    #test_filtrar_por_genero(lfn, 'Hombre')
+    #test_calcular_nombres(lfn, 'Mujer')
+    #test_calcular_top_nombres_de_año(lfn, 2005)
+    #test_calcular_nombres_ambos_generos(lfn)
+    #test_calcular_nombres_compuestos(lfn, 'Hombre')
+    #test_calcular_frecuencia_media_nombre_años(lfn, 'RAFAEL', 2002, 2006)
+    #test_calcular_nombre_mas_frecuente_año_genero(lfn, 2005, 'Hombre')
+    test_calcular_año_mas_frecuencia_nombre(lfn, 'HUGO')
